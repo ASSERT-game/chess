1c8563c0dd55c3a038888be228df766e7f149198
Kevin Colour <kecolour@gmail.com>
2020-06-17 22:40:08 -0700

    Pawn Movement done

--- src/moveset/moveset_pawn.c
+++ src/moveset/moveset_pawn.c
@@ -6,7 +6,7 @@
 /*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 04:59:15 by home              #+#    #+#             */
-/*   Updated: 2020/06/17 05:02:52 by home             ###   ########.fr       */
+/*   Updated: 2020/06/17 22:38:23 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,43 @@
 
 void	toggle_pawn_moveset(t_game_state *game_state)
 {
-	(void)game_state;
+	int		i;
+	int		try[4];
+	int		delta;
+
+	i = screen_to_board(game_state->select_x, game_state->select_y);
+	memset_pattern4(try, &i, sizeof(try));
+
+	delta = 1;
+	if (game_state->turn % 2 == WHITE_TURN)
+		delta = -1;
+
+	move_by(&(try[0]),  0, (delta));
+	if ( game_state->map[try[0]] != NONE)
+		try[0] = OUT_OF_BOUNDS;
+
+	move_by(&(try[1]),  0, (delta) * 2);
+	if (game_state->turn % 2 == WHITE_TURN && i / 8 != 6)
+		try[1] = OUT_OF_BOUNDS;
+	if (game_state->turn % 2 == BLACK_TURN && i / 8 != 1)
+		try[1] = OUT_OF_BOUNDS;
+
+	move_by(&(try[2]), -1, (delta));
+	move_by(&(try[3]),  1, (delta));
+	if (try[2] != OUT_OF_BOUNDS && game_state->map[try[2]] == NONE)
+		try[2] = OUT_OF_BOUNDS;
+	if (try[3] != OUT_OF_BOUNDS && game_state->map[try[3]] == NONE)
+		try[3] = OUT_OF_BOUNDS;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (try[i] != OUT_OF_BOUNDS)
+		{
+			game_state->possible_tiles[try[i]] = 1;
+			if (same_as_turn(game_state->map[try[i]], game_state->turn) == true)
+				game_state->possible_tiles[try[i]] = 0;
+		}
+		i++;
+	}
 }
